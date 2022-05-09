c3010a4944ef70d38e87739557c8771eedb3dac6
Yishan566 <royce_zhang@163.com>
2022-05-09 20:26:13 +0800

    remove helloworld comment

--- src/main/java/com/shan/springboot_start/SpringbootStartApplication.java
+++ src/main/java/com/shan/springboot_start/SpringbootStartApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootStartApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
         SpringApplication.run(SpringbootStartApplication.class, args);
     }
 
